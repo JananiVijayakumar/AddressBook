323714e428080ebd43e251ea55d80c8fa5130859
Janani <vsvprema@gmail.com>
2021-12-03 17:54:36 +0530

    Added : Multiple AddressBook to system

--- src/main/java/com/bridgelabz/AddressBookMain.java
+++ src/main/java/com/bridgelabz/AddressBookMain.java
@@ -2,6 +2,14 @@ package com.bridgelabz;
 
 public class AddressBookMain {
     public static void main(String[] args) {
-        System.out.println("Welcom to AddressBook program");
+        System.out.println("Welcome to AddressBook program");
+        AddressBook addressBook = new AddressBook();
+        addressBook.takeOption();
+        boolean condition = true;
+        while (condition) {
+            boolean checkUser = addressBook.takeOption();
+            if (checkUser)
+                break;
+        }
     }
 }
